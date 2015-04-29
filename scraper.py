4a0d5f9005b15db937208f52a0170036f9f14465
woodbine <ian@spendnetwork.com>
2015-04-29 22:40:39 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -28,6 +28,7 @@ links = soup.findAll('a', title=True)
 for link in links:
 	url = 'http://www.staffordshire.gov.uk' + link['href']
 	if 'expenditureexceeding500' in url:
+		print url
 		html2 = urllib2.urlopen(url)
 		soup2 = BeautifulSoup(html)
 		
